b067fbc878e6912e70a6d6b38f8164ebfe25aa96
Melbs0 <marcello.luis@aluno.ifsp.edu.br>
2022-12-15 20:59:22 -0300

    now i commited this

--- assets/js/pokeUI.js
+++ assets/js/pokeUI.js
@@ -18,6 +18,9 @@ function messToPOK (pok){
     det.img = pok.sprites.other.dream_world.front_default
     det.ablts= pok.abilities
     det.moves = pok.moves 
+    det.types = [pok.types[0].type.name, pok.types[1].type.name]
+    det.weight = Number(pok.weight)
+    det.height = Number(pok.height)
     return det
 
 
@@ -30,31 +33,44 @@ function pegaOnomeAe(algo){
 function newHtml (algoai){
 return `
 <div class='RightContainer'>
-${algoai.nome}
-
-<ol class = 'stats'>
-<li>${algoai.stats[0]}</li>
-<li>${algoai.stats[1]}</li>
-<li>${algoai.stats[2]}</li>
-<li>${algoai.stats[3]}</li>
-<li>${algoai.stats[4]}</li>
-<li>${algoai.stats[5]}</li>
-</ol>
-
+    <h1>${algoai.nome}</h1>
+    <img src= '${algoai.img}' alt = "imagem pokemon" class = "img"/>
 </div>
 
 <div class = 'LeftContainer'>
-<img src= '${algoai.img}' alt = "imagem pokemon"/>
+    <div class = "contain">
+        <h2>Stats</h2>
+        <ol class = 'stats'>
+            <li class = "listItem">HP: ${algoai.stats[0]}</li>
+                <hr/>
+            <li class = "listItem">Atack: ${algoai.stats[1]}</li>
+                <hr/>
+            <li class = "listItem">Defense: ${algoai.stats[2]}</li>
+                <hr/>
+            <li class = "listItem">Especial Atack: ${algoai.stats[3]}</li>
+                <hr/>
+            <li class = "listItem">Especial Defense: ${algoai.stats[4]}</li>
+                <hr/>
+            <li class = "listItem">Speed: ${algoai.stats[5]}</li>
+        </ol>
+        </div>
+        
+        
+<div class = "contain">
+        <h2>About</h2>
+        <ol class =  "other">
+            <li class = "listItem">tipos:</li>
+                <hr/>
+            <li class = "listItem"> ${algoai.types}</li>
+                <hr/>
+            <li class = "listItem">outros:</li>
+                <hr/>
+            <li class = "listItem">peso: ${algoai.weight/10}Kg</li>
+                <hr/>
+            <li class = "listItem">altura: ${algoai.height/10}m</li> 
+        </ol>
+    </div>
 </div>
-${algoai.abilities}
-${algoai.moves}
-
-
-
-
-
-
-
 `
 }
 
@@ -69,11 +85,3 @@ const sla = arrumaí().then((poke=[])=>{
     const html = newHtml(poke)
     div.innerHTML = html
 })
-
-console.log(sla)
-
-
-
-
-
-
